daad6527dd2dd352ab9e59604a716399f4f3a154
Luke Van In <lukevanin@gmail.com>
2020-02-24 14:17:28 +0200

    Add One.py for exercise 1.

---
+++ One.py
@@ -0,0 +1,11 @@
+"""Prompt user to enter a URL:
+
+1. Verify the string entered is a valid URL, otherwise return an error message
+2. If it is a valid URL, display a dictionary with the following keys:
+
+a. “domain_name”: returns the domain name of the URL
+b. “protocol:” http or https
+c. “title”: the <title> of the page
+d. “image”: the URLs of all the images <img>
+e. "stylesheets": the number of stylesheets present in the html of the page"""
+
