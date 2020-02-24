76c2ea445e3c26def38a01cb418e9e30decc8041
Luke Van In <lukevanin@gmail.com>
2020-02-24 15:40:27 +0200

    Parse title

--- utils/parser.py
+++ utils/parser.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 class Parser:
     def parse(self, html: str) -> dict:
         soup = BeautifulSoup(markup=html, features='html.parser')
+        title = soup.title
         return {
-            'title': '',
+            'title': title.string if title else '',
             'image_urls': [],
             'stylesheet_count': 0,
         }
--- utils/tests/parser_test.py
+++ utils/tests/parser_test.py
@@ -1,9 +1,21 @@
+from typing import List
+
 from ..parser import Parser
+
+
+def check_result(result: dict, title: str, image_urls: List[str], stylesheet_count: int):
+    assert(result['title'] == title)
+    assert(result['image_urls'] == image_urls)
+    assert(result['stylesheet_count'] == stylesheet_count)
 
 
 def test_parse_empty_doc():
     doc = """"""
-    p = Parser().parse(doc)
-    assert(p['title'] == '')
-    assert(p['image_urls'] == [])
-    assert(p['stylesheet_count'] == 0)
+    res = Parser().parse(doc)
+    check_result(res, title='', image_urls=[], stylesheet_count=0)
+
+
+def test_parse_title():
+    doc = """<html><head><title>Foo</title></head></html>"""
+    res = Parser().parse(doc)
+    check_result(res, title='Foo', image_urls=[], stylesheet_count=0)
